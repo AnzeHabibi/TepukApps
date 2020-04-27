f74bbe1625ed9aa6f517614f87897f848c64877c
ojan <ramadhan_tiba@apps.ipb.ac.id>
2020-04-27 15:08:20 +0700

    Fix Signin

--- app/src/main/java/com/example/tepukapps/Signin.java
+++ app/src/main/java/com/example/tepukapps/Signin.java
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Signin extends AppCompatActivity {
 
     TextView btn_new_account;
     Button btn_signin;
-    EditText xusername, xpassword;
+    TextInputEditText textUsername, textPassword;
+    TextInputLayout layoutUsername,layoutPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +28,11 @@ public class Signin extends AppCompatActivity {
         setContentView(R.layout.activity_signin);
 
         btn_new_account = findViewById(R.id.btn_new_account);
-        xusername = findViewById(R.id.xusername);
+        textUsername = findViewById(R.id.xusername);
+        layoutUsername = findViewById(R.id.layoutUsername);
         btn_signin = findViewById(R.id.btn_signin);
-        xpassword = findViewById(R.id.xpassword);
+        textPassword = findViewById(R.id.xpassword);
+        layoutPassword = findViewById(R.id.layoutPassword);
 
         btn_new_account.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,9 +45,71 @@ public class Signin extends AppCompatActivity {
         btn_signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent gotodetail = new Intent(Signin.this, DetailAct.class);
-                startActivity(gotodetail);
+                if (validate()) {
+                    Intent gotodetail = new Intent(Signin.this, DetailAct.class);
+                    startActivity(gotodetail);
+                }
             }
         });
+
+        textUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!textUsername.getText().toString().isEmpty()){
+                    layoutUsername.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        textPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(textPassword.getText().toString().length() > 7){
+                    layoutPassword.setErrorEnabled(false);
+                }
+                if(!textPassword.getText().toString().isEmpty()){
+                    layoutPassword.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+
+    private boolean validate(){
+        if(textUsername.getText().toString().isEmpty()){
+            layoutUsername.setErrorEnabled(true);
+            layoutUsername.setError("Username is Require");
+            return false;
+        }
+        if(textPassword.getText().toString().length() < 8 ){
+            layoutPassword.setErrorEnabled(true);
+            layoutPassword.setError("Password minimum 8 characters");
+            return false;
+        }
+        if(textPassword.getText().toString().isEmpty()){
+            layoutPassword.setErrorEnabled(true);
+            layoutPassword.setError("Password is Require");
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/tepukapps/Signup.java
+++ app/src/main/java/com/example/tepukapps/Signup.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Signup extends AppCompatActivity {
 
     Button btn_continue;
     LinearLayout btn_back;
-    EditText username, password, email;
+    TextInputEditText username, password, email;
+    TextInputLayout layoutUsername,layoutPassword,layoutEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,9 @@ public class Signup extends AppCompatActivity {
         username=findViewById(R.id.xusername);
         password=findViewById(R.id.xpassword);
         email=findViewById(R.id.email);
+        layoutEmail = findViewById(R.id.layoutEmail);
+        layoutPassword = findViewById(R.id.layoutPassword);
+        layoutUsername = findViewById(R.id.layoutUsername);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,9 +47,93 @@ public class Signup extends AppCompatActivity {
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent go = new Intent(Signup.this, SignUp2.class);
-                startActivity(go);
+                if (validate()) {
+                    Intent go = new Intent(Signup.this, SignUp2.class);
+                    startActivity(go);
+                }
             }
         });
+
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!username.getText().toString().isEmpty()){
+                    layoutUsername.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(password.getText().toString().length() > 7){
+                    layoutPassword.setErrorEnabled(false);
+                }
+                if(!password.getText().toString().isEmpty()){
+                    layoutPassword.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!email.getText().toString().isEmpty()){
+                    layoutEmail.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+    }
+    private boolean validate(){
+        if(username.getText().toString().isEmpty()){
+            layoutUsername.setErrorEnabled(true);
+            layoutUsername.setError("Username is Require");
+            return false;
+        }
+        if(email.getText().toString().isEmpty()){
+            layoutEmail.setErrorEnabled(true);
+            layoutEmail.setError("Email is Require");
+            return false;
+        }
+        if(password.getText().toString().length() < 8 ){
+            layoutPassword.setErrorEnabled(true);
+            layoutPassword.setError("Password minimum 8 characters");
+            return false;
+        }
+        if(password.getText().toString().isEmpty()){
+            layoutPassword.setErrorEnabled(true);
+            layoutPassword.setError("Password is Require");
+            return false;
+        }
+        return true;
     }
 }
