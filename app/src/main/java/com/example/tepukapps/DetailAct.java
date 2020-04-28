cd8b46584bffead0797db4a978c32a228849d38c
abiyyu_habibi <abiyyu_habibi@apps.ipb.ac.id>
2020-04-28 10:26:21 +0700

    Detail java

--- app/src/main/java/com/example/tepukapps/DetailAct.java
+++ app/src/main/java/com/example/tepukapps/DetailAct.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,8 @@ public class DetailAct extends AppCompatActivity {
 
     Button btn_continue;
     LinearLayout btn_back;
+    ImageView img_pupuk;
+    TextView txt_harga, txt_stok, txt_jenis, txt_komposisi, txt_desc, title_pupuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,13 @@ public class DetailAct extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         btn_continue = findViewById(R.id.btn_continue);
+        txt_harga=findViewById(R.id.txt_harga);
+        txt_desc=findViewById(R.id.txt_desc);
+        txt_jenis=findViewById(R.id.txt_jenis);
+        txt_komposisi=findViewById(R.id.txt_komposisi);
+        txt_stok=findViewById(R.id.txt_stok);
+        title_pupuk=findViewById(R.id.title_pupuk);
+        img_pupuk=findViewById(R.id.img_pupuk);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
